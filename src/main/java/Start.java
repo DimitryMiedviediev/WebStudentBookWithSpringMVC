228af0031361bb793dc838437fb8e5cfb4f52b3b
Dimitry Miedviediev <dimitrymiedviediev@gmail.com>
2017-04-21 18:16:58 +0300

    Second Commit on Spring Project

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -6,5 +6,8 @@ public class Start {
         System.out.println("Hello Java");
         System.out.println("Hello World");
         System.out.println("Hello Java");
+        System.out.println("Hello Java");
+        System.out.println("Hello World");
+        System.out.println("Hello Java");
     }
 }
