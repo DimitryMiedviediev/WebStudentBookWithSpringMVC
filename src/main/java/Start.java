167e2c969e3a75396e461b881caf8d5c410d2f57
Dimitry Miedviediev <dimitrymiedviediev@gmail.com>
2017-04-21 18:15:57 +0300

    First Commit on Spring Project

---
+++ src/main/java/Start.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Dimitry on 21.04.17.
+ */
+public class Start {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+        System.out.println("Hello World");
+        System.out.println("Hello Java");
+    }
+}
