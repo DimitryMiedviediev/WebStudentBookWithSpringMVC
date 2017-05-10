a4f3b3185fc037b4df18b2cbef0f73ea249a683f
Dimitry Miedviediev <dimitrymiedviediev@gmail.com>
2017-05-10 13:54:39 +0300

    Ready with the xml configuration file

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,13 +1,36 @@
+import entity.Person;
+import inversionOfControl.Coach;
+import inversionOfControl.CricketCoach;
+import inversionOfControl.TrackCoach;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Created by Dimitry on 21.04.17.
  */
 public class Start {
     public static void main(String[] args) {
-        System.out.println("Hello Java");
-        System.out.println("Hello World");
-        System.out.println("Hello Java");
-        System.out.println("Hello Java");
-        System.out.println("Hello World");
-        System.out.println("Hello Java");
+
+
+        //Inversion of Control
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/beans/applicationContext.xml");
+
+        CricketCoach theCoach = context.getBean("cricketCoach", inversionOfControl.CricketCoach.class);
+
+        System.out.println(theCoach.getDailyWorkout());
+
+        System.out.println(theCoach.getDailyFortune());
+
+        System.out.println(theCoach.getName());
+
+        System.out.println(theCoach.getEmail());
+
+        context.close();
+
+//        Coach coach = new TrackCoach();
+//        System.out.println(coach);
+
+        //Dependencies Injections
+
     }
 }
